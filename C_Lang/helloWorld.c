87e1eb1d2cd0a14aa940fd6c3f3321c10a2b4197
Don99yu <jidongk@gmail.com>
2026-01-08 16:24:30 +0900

    add file commant

--- C_Lang/helloWorld.c
+++ C_Lang/helloWorld.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
+#include <stdio.h> //전처리기 standard input output header파일
 
-int main() 
+int main(void) // int타입의 main함수 매개변수는 비어있음 
 {
     printf("Hello, World!\n");
     return 0;
